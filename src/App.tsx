26f9a579c7c1b1f6bcf246203094dc38b56e33b2
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-18 22:30:59 +0000

    added basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Home } from "./components/home/Home";
+import { PageNotFound } from "./components/misc/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
 
