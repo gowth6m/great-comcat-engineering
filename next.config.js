80e665a6219dc600a8200f4e093808fcd6b9155d
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-19 19:30:26 +0000

    added product item

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'picsum.photos'] // <== Domain name
+  }
 }
-
 module.exports = nextConfig
---
+++ pages/product/[slug].tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import data from "../../utils/data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug);
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <Layout title={product.name}>
+      <div className="py-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        {/* Image */}
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </div>
+        {/* List on the right of image */}
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>
+              <h1 className="text-lg">{product.category}</h1>
+            </li>
+            <li>
+              <h1 className="text-lg">{product.rating}</h1>
+            </li>
+            <li>
+              <h1 className="text-lg">{product.description}</h1>
+            </li>
+          </ul>
+        </div>
+        {/* Below the right text */}
+        <div className="">
+          <div className="card p-5">
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>{product.price}</div>
+            </div>
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
